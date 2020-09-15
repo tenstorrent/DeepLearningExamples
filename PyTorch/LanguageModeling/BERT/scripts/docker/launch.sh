3ec9aa6cceae4e220373ae4a77e38183673ea749
Joy Chen <joychen17@gmail.com>
2020-09-14 23:25:21 -0400

    Update docker with external repo

--- PyTorch/LanguageModeling/BERT/scripts/docker/launch.sh
+++ PyTorch/LanguageModeling/BERT/scripts/docker/launch.sh
@@ -2,6 +2,7 @@
 
 NV_VISIBLE_DEVICES=${1:-"all"}
 DOCKER_BRIDGE=${3:-"host"}
+ROOT_DIR=`git rev-parse --show-toplevel`
 
 docker run -it --rm \
   --gpus device=$NV_VISIBLE_DEVICES \
@@ -10,6 +11,8 @@ docker run -it --rm \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   -e LD_LIBRARY_PATH='/workspace/install/lib/' \
+  -e PYTHONPATH=$PYTHONPATH:'/workspace/bert/models' \
   -v $PWD:/workspace/bert \
   -v $PWD/results:/results \
+  --mount type=bind,source=$ROOT_DIR/external-repositories/smekalka/smekalka/models,target=/workspace/bert/models \
   bert 
